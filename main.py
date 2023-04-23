ecfe5a23d3ec8e350454881d28da3fa0350377e9
Averines <averines@gmail.com>
2023-04-23 13:03:59 +0300

    add: page contacts

--- main.py
+++ main.py
@@ -13,5 +13,10 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
